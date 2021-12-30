8a2996baa4fdf7db90724ecb2a8088fc016dc824
k1105 <kanapon1105@yahoo.co.jp>
2021-12-31 00:23:06 +0900

    衝突判定の無限ループに入るバグの修正 #22

--- module/TriangleIntersectionDetection.py
+++ module/TriangleIntersectionDetection.py
@@ -9,16 +9,17 @@ def det3x3(a, b, c):
 def isIntersectToTriangle(triangle, line):
     # triangleとlineが交差しているかの判定を行う関数.
     # line: [重心, 頂点]
-    origin = np.array(line[0])  # 線分の開始点
-    ray = np.array(line[1]) - origin  # 線分の終点
-    invRay = -1 * ray  # 終点ベクトルの反対
+
     v0 = np.array(triangle[0])
     v1 = np.array(triangle[1])
     v2 = np.array(triangle[2])
 
-    # v0, v1, v2, およびorigin, rayに重複した要素が存在していないか検証. もし重複した要素が存在していない場合は以下を実行
-    if len(Set2D.set2D([v0, v1, v2]) & Set2D.set2D([origin, ray])) == 0:
-
+    # v0, v1, v2, およびline[0], line[1]に重複した要素が存在していないか検証. もし重複した要素が存在していない場合は以下を実行
+    dup = len(Set2D.set2D([v0, v1, v2]) & Set2D.set2D([line[0], line[1]]))
+    if dup == 0:
+        origin = np.array(line[0])  # 線分の開始点
+        ray = np.array(line[1]) - origin  # 線分の終点
+        invRay = -1 * ray  # 終点ベクトルの反対
         edge1 = v1 - v0
         edge2 = v2 - v0
 
