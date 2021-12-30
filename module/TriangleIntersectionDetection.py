915d646fe7242fbb3cd77bccc3cff8c215d2f1ec
k1105 <kanapon1105@yahoo.co.jp>
2021-12-30 23:28:00 +0900

    jsonファイルを入力にしてplane_collision.pyを実行できるよう修正

---
+++ module/TriangleIntersectionDetection.py
@@ -0,0 +1,58 @@
+import numpy as np
+from . import Set2D
+
+
+def det3x3(a, b, c):
+    return a[0]*b[1]*c[2]+a[1]*b[2]*c[0]+a[2]*b[0]*c[1]-a[2]*b[1]*c[0]-a[1]*b[0]*c[2]-a[0]*b[2]*c[1]
+
+
+def isIntersectToTriangle(triangle, line):
+    # triangleとlineが交差しているかの判定を行う関数.
+    # line: [重心, 頂点]
+    origin = np.array(line[0])  # 線分の開始点
+    ray = np.array(line[1]) - origin  # 線分の終点
+    invRay = -1 * ray  # 終点ベクトルの反対
+    v0 = np.array(triangle[0])
+    v1 = np.array(triangle[1])
+    v2 = np.array(triangle[2])
+
+    # v0, v1, v2, およびorigin, rayに重複した要素が存在していないか検証. もし重複した要素が存在していない場合は以下を実行
+    if len(Set2D.set2D([v0, v1, v2]) & Set2D.set2D([origin, ray])) == 0:
+
+        edge1 = v1 - v0
+        edge2 = v2 - v0
+
+        denominator = det3x3(edge1, edge2, invRay)
+
+        if denominator > 0:
+            d = origin - v0
+            u = det3x3(d, edge2, invRay) / denominator
+            if 0 <= u <= 1:
+                v = det3x3(edge1, d, invRay) / denominator
+
+                if 0 <= v and u + v <= 1:
+                    t = det3x3(edge1, edge2, d) / denominator
+
+                    # 距離がマイナスの場合は交差していない
+                    if t >= 0:
+                        return True
+
+        origin = np.array(line[1])  # 線分の開始点
+        ray = np.array(line[0]) - origin  # 線分の終点
+        invRay = -1 * ray  # 終点ベクトルの反対
+
+        denominator = det3x3(edge1, edge2, invRay)
+
+        if denominator > 0:
+            d = origin - v0
+            u = det3x3(d, edge2, invRay) / denominator
+            if 0 <= u <= 1:
+                v = det3x3(edge1, d, invRay) / denominator
+                if 0 <= v and u + v <= 1:
+                    t = det3x3(edge1, edge2, d) / denominator
+
+                    # 距離がマイナスの場合は交差していない
+                    if t >= 0:
+                        return True
+
+    return False
--- plane_collision.py
+++ plane_collision.py
@@ -1,4 +1,4 @@
-from module import PlaneCollisionDetection as pcol
+from module import TriangleIntersectionDetection as tint
 import numpy as np
 import json
 
@@ -9,20 +9,23 @@ import json
 # どのような場合にそうした誤った挙動をしてしまうのかについて, 様々な値を検討することで調査してほしい.
 # </sammary>
 
+i = int(input('利用する三角形の番号-> '))
+j = int(input('利用する線分の番号-> '))
+
 triangle_open = open('triangle.json', 'r')
 line_open = open('line.json', 'r')
 triangle = json.load(triangle_open)
 line = json.load(line_open)
 
-p0 = triangle['tri1']['p0']
-p1 = triangle['tri1']['p1']
-p2 = triangle['tri1']['p2']
+p0 = triangle['tri'+str(i)]['p0']
+p1 = triangle['tri'+str(i)]['p1']
+p2 = triangle['tri'+str(i)]['p2']
 
-start = line['line1']['start']
-end = line['line1']['end']
+start = line['line'+str(j)]['start']
+end = line['line'+str(j)]['end']
 
 # if len(p0) != 3 and len(p1) != 3 and len(p2) != 3 and len(start) != 3 and len(end) != 3:
-result = pcol.isIntersectToPlane([p0, p1, p2], [start, end])
+result = tint.isIntersectToTriangle([p0, p1, p2], [start, end])
 
 if result:
     print('衝突しています')
