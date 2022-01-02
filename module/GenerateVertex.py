aa0a63b5d6a8f83943115bdd17550a6fd4396005
k1105 <kanapon1105@yahoo.co.jp>
2022-01-02 21:19:45 +0900

    出力ベクトルの作成 #36

--- module/GenerateVertex.py
+++ module/GenerateVertex.py
@@ -1,9 +1,12 @@
 import numpy as np
 import random
 import math
+import numpy.linalg as LP
 
 
 def GenerateVertex(tetra, tetras, target, k, random_list):
+
+    pi = math.pi
 
     triangle = tetra.triangle[target]
     left_point = np.array(tetra.point[target])
@@ -12,16 +15,21 @@ def GenerateVertex(tetra, tetras, target, k, random_list):
         tetra.getPositionInformationVector(
             tetras)  # 入力ベクトル. 注目要素の近傍情報ベクトルと位置情報ベクトルのそれぞれにk, 1-k倍をして算出.
 
+    input_norm = LP.norm(input)
+
     # 0 < theta < 2pi
-    theta = math.atan2(input[1], input[0]) + math.pi
+    theta = math.atan2(input[1], input[0]) + pi
     # -pi/2 < phi < pi/2
-    phi = (math.atan2(input[2], input[1]) + math.pi) % math.pi - math.pi / 2
+    phi = (math.atan2(input[2], input[1]) + pi) % pi - pi / 2
 
     theta_num = int(theta / (math.pi/4)) + 1
-    phi_num = int((phi + math.pi / 2) / (math.pi / 4)) + 1
+    phi_num = int((phi + pi / 2) / (pi / 4)) + 1
 
-    print(str(theta_num) + ' -> ' + str(random_list["theta"][theta_num-1]))
-    print(str(phi_num) + ' -> ' + str(random_list["phi"][phi_num-1]))
+    out_theta = theta + random_list["theta"][theta_num-1] * pi / 4
+    out_phi = phi + random_list["phi"][phi_num-1] * pi / 8
+
+    output = input_norm * np.array([math.cos(out_phi)*math.cos(out_theta),
+                                    math.cos(out_phi)*math.sin(out_theta), math.sin(out_phi)])
 
     # ベクトルを作る
     center = (np.array(triangle[0])+np.array(triangle[1]) +
