651c95ea28c52c3698cf7f747de3a3a824e38764
k1105 <kanapon1105@yahoo.co.jp>
2022-01-21 14:33:13 +0900

    bugfix: phiの状態番号の取得ロジックを修正 #60

--- module/GenerateVertex.py
+++ module/GenerateVertex.py
@@ -13,16 +13,31 @@ def GenerateVertex(tetra, tetras, k, vert, random_list):
 
     input_norm = LP.norm(input)
 
-    # 0 < theta < 2pi
-    theta = (math.atan2(input[1], input[0]) + pi) % (pi / 2)
-    # -pi/2 < phi < pi/2
-    phi = (math.atan2(input[2], input[1]) + pi) % pi - pi / 2
+    # 0 <= theta < 2pi
+    theta = (math.atan2(input[1], input[0]) + 2 * pi) % (2 * pi)
+    # -pi/2 <= phi < pi/2
+    phi = math.atan2(input[2], abs(input[1]))
 
-    theta_num = int(theta / (math.pi/4)) + 1
-    phi_num = int((phi + pi / 2) / (pi / 4)) + 1
+    if 0 <= theta < 2*pi:  # 定義域内であることの確認
+        theta_num = int(theta / (pi / 4)) + 1
+    else:
+        print("theta error!")
+        print(str(theta / pi) + "pi")
 
-    out_theta = theta + random_list["theta"][theta_num-1] * pi / 4
-    out_phi = phi + random_list["phi"][phi_num-1] * pi / 8
+    if -pi/2 <= phi < pi/2:  # 定義域内であることの確認
+        phi_num = int((phi + pi / 2) / (pi / 4)) + 1
+    else:
+        print("phi error!")
+
+    if 1 <= theta_num <= 8:
+        out_theta = theta + random_list["theta"][theta_num-1] * pi / 4
+    else:
+        print("theta_num error!")
+
+    if 1 <= phi_num <= 4:
+        out_phi = phi + random_list["phi"][phi_num-1] * pi / 8
+    else:
+        print("phi_num error!")
 
     output = input_norm * np.array([math.cos(out_phi)*math.cos(out_theta),
                                     math.cos(out_phi)*math.sin(out_theta), math.sin(out_phi)])
